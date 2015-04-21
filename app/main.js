96fcf2cbd0a6405041e683d2defa5fe6d65b2e68
Eric Drechsel <eric@pdxhub.org>
2015-04-21 16:49:13 -0700

    Install superagent

--- app/main.js
+++ app/main.js
@@ -2,8 +2,20 @@
 
 import React from 'react';
 import Hello from 'hello';
+import request from 'superagent';
 
 React.render(
   <Hello name="TiddlyWeb"/>,
   document.getElementById('main')
 );
+
+request
+  .get('https://tank.peermore.com/bags.json')
+  .set('Accept', 'application/json')
+  .end(function(err, res){
+    if (res.ok) {
+      console.log(res.body);
+    } else {
+      alert('Oh no! error ' + res.text);
+    }
+  });
